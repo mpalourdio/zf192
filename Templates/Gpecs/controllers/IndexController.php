836bc4d24b05706cda5637d6733ab0db6d1465a0
mpalourdio <mpalourdio@gmail.com>
2015-04-17 00:11:05 +0200

    poc 1 => templates sortis, modules, controllers, classes etc.

---
+++ Templates/Gpecs/controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+class Gpecs_IndexController extends Zend_Controller_Action
+{
+    function indexAction()
+    {
+        $a = new Templates_Gpecs_Lib_Dir_Test();
+        $this->view->title = "gpecs controller";
+    }
+
+    function ajouterAction()
+    {
+        $this->view->title = "Ajouter un nouvel album";
+    }
+
+    function modifierAction()
+    {
+        $this->view->title = "Modifier un album";
+    }
+
+    function supprimerAction()
+    {
+        $this->view->title = "Supprimer un album";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+define('APPLICATION_ENV',getenv('APPLICATION_ENV'));
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+date_default_timezone_set('Europe/Paris');
+
+// mise en place des répertoires et chargement des classes
+set_include_path(
+    '.'
+    . PATH_SEPARATOR . '../library'
+    . PATH_SEPARATOR . '../'
+    . PATH_SEPARATOR . '../application/models/'
+    . PATH_SEPARATOR . get_include_path()
+);
+
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('Templates_');
+
+
+// setup controller
+$frontController = Zend_Controller_Front::getInstance();
+$frontController->throwExceptions(true);
+$frontController->setControllerDirectory('../application/controllers');
+
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/config/application.ini'
+);
+$application->bootstrap()->run();
